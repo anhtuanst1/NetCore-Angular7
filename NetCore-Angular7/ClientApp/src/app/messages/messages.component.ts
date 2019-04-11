da4dbf1360ef576418d9f9abfa4c64d036f35d21
phạm anh tuấn <anhtuanst1@gmail.com>
2019-04-12 00:07:35 +0700

    Update tutorial.

---
+++ NetCore-Angular7/ClientApp/src/app/messages/messages.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  constructor(public messageService: MessageService) {}
+
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+
+  ngOnInit() {
+    this.scrollToBottom();
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
+
+  ClearMessage() {
+    this.messageService.clear();
+  }
+
+}
